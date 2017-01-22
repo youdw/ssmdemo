684b7d1bdf3cf118f18308d7368bcd2485ad3d78
youdw <eastyoudw@gmail.com>
2017-01-22 17:01:09 +0800

    测试GIF图片压缩

---
+++ src/test/java/cc/youdw/service/ImageTest.java
@@ -0,0 +1,24 @@
+package cc.youdw.service;
+
+import cc.youdw.common.BaseTest;
+import net.coobird.thumbnailator.Thumbnails;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * GIF 图片压缩
+ * Created by east on 2017/1/22.
+ */
+public class ImageTest extends BaseTest {
+
+    @Test
+    public void resize() throws IOException {
+        File imageFile = new File("/Users/east/Desktop/giftest.gif");
+        File toFile = new File("/Users/east/Desktop/giftest2.gif");
+        Thumbnails.of(imageFile).size(100,100)
+                .toFile(toFile);
+    }
+
+}
